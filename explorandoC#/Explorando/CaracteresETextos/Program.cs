c6ddf16099d284a42fe671bd95287a364a88a47f
Ester Silva <ester.silva@movidesk.com>
2022-08-01 17:54:53 -0300

    atualizar

---
+++ explorandoC#/Explorando/CaracteresETextos/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Programa
+{
+    static void Main(string[] args)
+    {
+        static void Main(string[] args)
+        {
+            char letra = 'a'; /* char  é uma variavel usada para letra */
+            Console.WriteLine(letra);
+
+
+            Console.WriteLine("tecla enter para fechar ..");
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ explorandoC#/Explorando/ConversoesEOutros-tipos/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Programa
+{
+    static void Main(string[] args)
+    {
+        static void Main(string[] args)
+        {
+            double salario;
+            salario = 3000.15;
+
+
+            int salarioInteiro;
+            salarioInteiro =(int) salario; /* Aqui esta fazendo o tipo quest  convertendo as variaveis  do tipo float  para  int  */
+
+            long x;
+            x = 2000000000000000000;/* armasena em torno de 64 bits */
+
+            short y;
+            y = 15000;/* variavel do tipo curto  vai armasenar em torno de 16 bits */
+
+            float altura =  1.62f; /* Desta forma o codigo fica um pouco mais limpo  sem o f de float o compilador vai pedir para usar um double */ 
+             
+            Console.WriteLine("tecla enter para fechar ..");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
