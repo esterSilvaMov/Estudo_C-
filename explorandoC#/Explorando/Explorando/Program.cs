b5f3f8ed2924b61933abdbae7281153f6c169866
Ester Silva <ester.silva@movidesk.com>
2022-08-01 14:28:16 -0300

    seguindo o plano de estudos

---
+++ explorandoC#/Explorando/Explorando/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("criando variavel ");
+        int idade;
+        idade = 33;
+        Console.WriteLine(idade);
+
+        idade = 33 - 5;
+        Console.WriteLine(idade);
+        idade = 5 * 2 - 6;
+        Console.WriteLine(idade);
+
+        Console.WriteLine( "tecla enter para fechar ..");
+
+        Console.ReadLine();
+    }
+}
---
+++ explorandoC#/Explorando/criandoVariaveis.flutuates/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 Variavel. flutuante ");
+        double salario; /* este tipo de variavel permite armasenar numeros com virgulas */
+        salario = 3000.13;
+        double idade;
+        idade = 7.5 / 3; /* para não perde a propriedade de  do numero inteiro  sempre ponha o , 0  caso não seja quebrado */
+        Console.WriteLine( idade    );
+        Console.WriteLine(salario);
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
