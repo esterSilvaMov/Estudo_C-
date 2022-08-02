1bcf41220496d8389bae7f3b37af84550f20771d
Ester Silva <ester.silva@movidesk.com>
2022-08-02 14:00:59 -0300

    Finalizando   curso

---
+++ explorandoC#/Explorando/encadeandoFor/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("encadeando for  ");
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas ++ )
+            // que ele venha contando linha a l,inha 
+        {
+            for (int contadorColunas =0; contadorColunas <10; contadorColunas ++ )
+            {
+                Console.Write("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+            }
+            Console.WriteLine( );
+        }
+
+
+
+
+        Console.WriteLine("tecla enter para fechar ..");
+
+        Console.ReadLine();
+    }
+}
+
---
+++ explorandoC#/Explorando/investimentoALongoPraso/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Laço de Repetição for  ");
+
+        double fatorRendimento = 1.005;
+        double investimento = 1000; 
+
+        for(int anos = 1; anos <= 5; anos++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
+            fatorRendimento += 0.001;
+
+            Console.WriteLine("depois de 5 anos voce tera  " + investimento );
+        }
+
+       
+
+
+
+        Console.WriteLine("tecla enter para fechar ..");
+
+        Console.ReadLine();
+    }
+}
+
