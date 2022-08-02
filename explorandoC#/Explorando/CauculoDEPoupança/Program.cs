e5ff6a1ce767ad78c1ad7758379bd07d142deff2
Ester Silva <ester.silva@movidesk.com>
2022-08-02 09:26:53 -0300

    for

---
+++ explorandoC#/Explorando/CauculoDEPoupança/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Caucula Poupança  ");
+        double investimento  = 1000;
+        /*Rendimento de 0,5%  convertido ao codigo  (0.005)ao mÊs */
+
+        investimento = investimento + investimento * 0.005;
+        //Console.WriteLine(investimento);
+        int mes = 1;
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine(investimento);
+            // mes = mes +1;
+            //mes ++ 1;
+            mes =+ 1;
+        }
+        Console.WriteLine("Rendimento total da sua poupança ");
+       
+
+
+        Console.WriteLine("tecla enter para fechar ..");
+
+        Console.ReadLine();
+    }
+}
---
+++ explorandoC#/Explorando/laçoDeRepetiçãoFor/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Laço de Repetição for  ");
+
+        double investimento = 1000;
+
+        for (int mes = 1; mes <=12; mes = mes + 1 )
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês" + mes + "você tem R$: " + investimento );
+        }
+   
+       
+
+
+
+        Console.WriteLine("tecla enter para fechar ..");
+
+        Console.ReadLine();
+    }
+}
+
